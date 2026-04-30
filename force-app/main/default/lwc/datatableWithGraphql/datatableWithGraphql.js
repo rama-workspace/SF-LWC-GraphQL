36d424644bb2b4bed2d984410db3725a3f1fac7e
ramakrishna.workspace <ramakrishna.workspace@gmail.com>
2026-05-01 05:25:43 +1200

    Initial Commit

---
+++ force-app/main/default/lwc/datatableWithGraphql/datatableWithGraphql.js
@@ -0,0 +1,56 @@
+// datatableWithGraphql.js
+import { LightningElement, wire } from "lwc";
+import { gql, graphql } from "lightning/graphql";
+
+const columns = [
+  { label: "Name", fieldName: "Name" },
+  { label: "Phone", fieldName: "Phone", type: "phone" },
+  { label: "Title", fieldName: "Title", type: "text" },
+];
+
+export default class DatatableWithGraphql extends LightningElement {
+  // Array of accounts to display
+  contacts = undefined;
+  // Errors from the wire adapter
+  errors = undefined;
+  // Columns for datatable
+  columns = columns;
+
+  @wire(graphql, {
+    query: gql`
+      query getContacts {
+        uiapi {
+          query {
+            Contact {
+              edges {
+                node {
+                  Id
+                  Name {
+                    value
+                  }
+                  Phone {
+                    value
+                  }
+                  Title {
+                    value
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+  })
+  gqlQuery({ data, errors }) {
+    if (data) {
+      this.contacts = data.uiapi.query.Contact.edges.map((edge) => ({
+        Id: edge.node.Id,
+        Name: edge.node.Name.value,
+        Phone: edge.node.Phone.value,
+        Title: edge.node.Title.value
+      }));
+    }
+    this.errors = errors;
+  }
+}
